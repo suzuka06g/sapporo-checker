4c786d846146f81e4f5e8a93741288f51740592d
suzuka06g <2006suzuka0822@gmail.com>
2026-07-29 10:39:08 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("サッポロチェッカーが起動しました！");
+const { chromium } = require("playwright");
+
+(async () => {
+  const browser = await chromium.launch({
+    headless: true
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto(
+    "https://yoyaku.harp.lg.jp/sapporo/",
+    {
+      waitUntil: "networkidle"
+    }
+  );
+
+  console.log("ページを開きました！");
+  console.log("タイトル:", await page.title());
+
+  await browser.close();
+})();
